7ba6d0396265e3a2f40b2fafd424058db658c4c3
Rui Dias <rui.dias10@gmail.com>
2025-01-28 10:49:30 +0000

    Update tmux

--- tmux/.tmux/scripts/status.sh
+++ tmux/.tmux/scripts/status.sh
@@ -1,30 +1,40 @@
 #!/bin/bash
 get_git_status() {
   CURRENT_DIR=$(tmux display-message -p "#{pane_current_path}")
-
   WINDOW_NAME=$(tmux display-message -p '#W' | xargs)
+  ZOOMED=$(tmux display-message -p "#{window_zoomed_flag}")
+
+  ZOOM_STATUS=""
+  if [ "$ZOOMED" -eq 1 ]; then
+    ZOOM_STATUS=" #[fg=#8a8c95]󰘖 zoom #[fg=#8a8c95] "
+  fi
 
   if [ "$WINDOW_NAME" = "ssh" ]; then
     IP=$(ps -t $(tmux display-message -p '#{pane_tty}') | grep ssh | awk '{print $5}' | tr -d '()')
-    echo " #[fg=#8a8c95]$WINDOW_NAME #[fg=#F0C674]$IP #[fg=#8a8c95] "
+    echo " $ZOOM_STATUS #[fg=#8a8c95] $WINDOW_NAME #[fg=#F0C674]$IP #[fg=#8a8c95] "
 
   elif [ "$WINDOW_NAME" = "docker" ]; then
-    echo " #[fg=#8a8c95]$WINDOW_NAME 󰡨 "
+    echo " $ZOOM_STATUS #[fg=#8a8c95]󰡨 $WINDOW_NAME $HOST_PID "
 
   elif [ "$WINDOW_NAME" = "ftp" ] || [ "$WINDOW_NAME" = "gftp" ]; then
-    echo " #[fg=#8a8c95]ftp  "
+    echo " $ZOOM_STATUS #[fg=#8a8c95] ftp "
 
   elif [ "$WINDOW_NAME" = "tmux" ] || [ "$WINDOW_NAME" = "[tmux]" ]; then
-    echo " #[fg=#8a8c95]tmux  "
+    echo " $ZOOM_STATUS #[fg=#8a8c95] tmux "
 
   elif [ -d "$CURRENT_DIR/.git" ]; then
-    echo " #[fg=#8a8c95]git  "
+    echo " $ZOOM_STATUS #[fg=#8a8c95] git "
 
   elif [ "$WINDOW_NAME" = "nvim" ]; then
-    echo " #[fg=#8a8c95]$WINDOW_NAME  "
+    echo " $ZOOM_STATUS #[fg=#8a8c95] $WINDOW_NAME "
+
+  elif [ "$WINDOW_NAME" = "sudo" ]; then
+    WINDOW_PANE_PID=$(tmux display-message -p "#{pane_pid}")
+    RUNNING_PROCESS=$(ps -o comm,etime= -p "$WINDOW_PANE_PID" | awk 'NR==2 {gsub(/^-/, ""); sub(/:[0-9]{2}$/, ""); print}')
+    echo " $ZOOM_STATUS #[fg=#8a8c95] $RUNNING_PROCESS "
 
   else
-    echo " #[fg=#8a8c95]$WINDOW_NAME  "
+    echo " $ZOOM_STATUS #[fg=#8a8c95] $WINDOW_NAME "
   fi
 }
 
